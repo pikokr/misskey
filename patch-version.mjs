283cbfb8a27e1d65a8f351ea4ffab1b7a99946ec
Pariring <paring@pikokr.dev>
2022-12-27 12:47:01 +0900

    fix: version format

--- patch-version.mjs
+++ patch-version.mjs
@@ -4,6 +4,6 @@ import fs from 'fs'
 
 if (!process.env.DOCKER) throw new Error('Don\'t run this script on dev environment')
 
-packageJson.version = `paring.${(await $`git rev-parse --short HEAD`).toString().trim()}`
+packageJson.version = `${packageJson.version}+paring-${(await $`git rev-parse --short HEAD`).toString().trim()}`
 
 await fs.promises.writeFile('package.json', JSON.stringify(packageJson))
