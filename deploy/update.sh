27670ac7cdb4e63d81b57b5cf1521d7e96593ab2
Pariring <paring@pikokr.dev>
2022-12-27 15:24:35 +0900

    fix: deploy issue

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,5 +1,3 @@
-echo -n $KUBE_CA > ca.crt
-
-curl --cacert ca.crt -H "Authorization: Bearer $API_TOKEN" $API_ENDPOINT/apis/apps/v1/namespaces/misskey/deployments/web \
+curl --insecure -H "Authorization: Bearer $API_TOKEN" $API_ENDPOINT/apis/apps/v1/namespaces/misskey/deployments/web \
 	-X PATCH -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" \
 	--data '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubrnetes.io/restartedAt":"'"$(date +%Y-%m-%dT%T%z)"'"}}}}}' > /dev/null
